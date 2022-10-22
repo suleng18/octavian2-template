80fe658f8e7bcf9605bb2e04159fd762ef0be163
Suleng18 <90331457+suleng18@users.noreply.github.com>
2022-10-22 12:52:50 +0700

    handle toggle menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuOpen = document.querySelector(".menu-toggle");
-  const menuClose = document.querySelector(".menu-close");
-  const menu = document.querySelector(".menu");
-  if (!menuOpen || !menuClose || !menu) return;
-  menuOpen.addEventListener("click", function () {
-    menu.classList.add("is-active");
-  });
-  menuClose.addEventListener("click", function () {
-    menu.classList.remove("is-active");
-  });
-  document.addEventListener("click", function (e) {
-    if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
-      menu.classList.remove("is-active");
+document.addEventListener('DOMContentLoaded', function (e) {
+  const menu = document.querySelector('.menu');
+  const menuOpen = document.querySelector('.menu-toggle');
+  const menuClose = document.querySelector('.menu-close');
+  if (!menuClose || !menuOpen || !menu) return;
+
+  document.addEventListener('click', function (e) {
+    if (!menu.contains(e.target) && !e.target.matches('.menu-toggle')) {
+      menu.classList.remove('is-active');
     }
+  });
+
+  menuOpen.addEventListener('click', function (e) {
+    menu.classList.add('is-active');
+  });
+
+  menuClose.addEventListener('click', function (e) {
+    menu.classList.remove('is-active');
   });
 });
